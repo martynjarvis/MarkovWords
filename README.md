f3fbf486ffed961ec24534684259c1dfe197d9ae
Martyn Jarvis <martynjarvis@gmail.com>
2014-11-24 15:46:38 +0000

    Modified to use collections rather than dict+list

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ MarkovWords
 
 Markov chain word generator.
 
-Examples:
+Examples generated from /usr/share/dict/words:
 porystlend
 cinnaccia
 surous
@@ -11,3 +11,13 @@ uneathologeress
 bris
 chafflike
 bonwrok
+
+Examples generated from /usr/share/dict/propernames:
+Guntong
+Jes
+Breanie
+Krista
+Rajany
+Hari
+Gathi
+Jarlen
--- markovwords.py
+++ markovwords.py
@@ -1,33 +1,44 @@
 import random
 import sys
 import pickle
+import collections
 
 FILENAME = 'lookup.p'
+
+# TODO lookup should be a default dict, with a Counter in it
 
 def generate_lookup(text_file):
     f = open(text_file)
     # (k,k) : [val,val,....]
-    lookup = {}
+    lookup = collections.defaultdict(collections.Counter)
     for word in f.readlines():
         i = j = None
         for k in word.strip().lower():
-            lookup.setdefault((i,j),[]).append(k)
+            lookup[(i,j)][k] += 1
             i = j
             j = k
-        lookup.setdefault((i,j),[]).append(None)
+        lookup[(i,j)][None] += 1
     pickle.dump(lookup, open( FILENAME, "wb" ))
+
+def random_from_generator(elements):
+    '''http://www.perlmonks.org/?node_id=1910'''
+    return_val = None
+    for n,e in enumerate(elements, start=1):
+        if random.random() < 1.0/n:
+            return_val = e
+    return return_val
 
 def generate_words(n):
     lookup = pickle.load(open( FILENAME, "rb" ))
     for i in range(n):
         i = j = None
-        k = random.choice(lookup[(i,j)])
+        k = random_from_generator(lookup[(i,j)].elements())
         word = []
         while k is not None:
             word.append(k)
             i = j
             j = k
-            k = random.choice(lookup[(i,j)])
+            k = random_from_generator(lookup[(i,j)].elements())
         print ''.join(word)
 
 if __name__=='__main__':
